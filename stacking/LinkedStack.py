7020f57c314ac35e0a3ce1c9c7f5625bf900993a
luk3ya0 <oneTOinf@163.com>
2023-09-26 01:44:08 +0800

    add simple `LinkedStack`

---
+++ stacking/LinkedStack.py
@@ -0,0 +1,34 @@
+from atomizing.Nodes import LinkedNode
+from atomizing.Types import Comparable
+
+
+class LinkedStack(object):
+    def __init__(self):
+        self.top = LinkedNode()  # End sentinel
+
+    def push(self, value: Comparable):
+        self.top = LinkedNode(value, self.top)
+
+    def pop(self) -> Comparable:
+        result: Comparable = self.top.value
+
+        if not self.top.end():
+            self.top = self.top.successor
+
+        return result
+
+
+if __name__ == '__main__':
+    linkedStackStr = LinkedStack()
+    for s in "Phasers on stun!".split():
+        linkedStackStr.push(s)
+
+    while not linkedStackStr.top.end():
+        print(linkedStackStr.pop())
+
+    linkedStackInt = LinkedStack()
+    for i in range(5):
+        linkedStackInt.push(i)
+
+    while not linkedStackInt.top.end():
+        print(linkedStackInt.pop())
